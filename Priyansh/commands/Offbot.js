8edab0e30c6bb7e947c898ccca15ab0652444716
Lord9s <nkunajunior399@gmail.com>
2024-12-04 10:43:21 +0200

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -2,13 +2,13 @@ module.exports.config = {
 	name: "offbot",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘓𝘰𝘳𝘥 𝘒𝘪𝘯𝘨",
 	description: "turn the bot off",
 	commandCategory: "system",
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
     const permission = ["100037743553265", "100037743553265"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Priyansh", event.threadID, event.messageID);
-  api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
+  	if (!permission.includes(event.senderID)) return api.sendMessage("Really bro You don't have permission to use this command, This Command Only For my owner Lord king", event.threadID, event.messageID);
+  api.sendMessage(`(ुŏ̥̥ŏ̥̥) ${global.config.BOTNAME} Bot is now turned off see you later Cia.`,event.threadID, () =>process.exit(0))
 }
